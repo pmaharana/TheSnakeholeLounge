f640dc422c6dacf43ebf6a2282611a4216521ec0
Pranay Maharana <pranay@Pranays-MBP.netgear.com>
2019-04-21 04:27:19 -0400

    ENV configs

--- util/database.js
+++ util/database.js
@@ -1,12 +1,12 @@
 const mysql   = require('mysql2'),
-      dotenv  = require('dotenv');
+      dotenv  = require('dotenv').config();
 
 const pool = mysql.createPool({
   connectionLimit : process.env.DB_CONLIMIT,
   host            : process.env.DB_HOST,
   user            : process.env.DB_USER ,
-  password        : process.env.DB_PASSWORD ,
-  database        : process.env.DB_DATABASE 
+  database        : process.env.DB_DATABASE,
+  password        : process.env.DB_PASSWORD
 });
 
 module.exports = pool.promise();
